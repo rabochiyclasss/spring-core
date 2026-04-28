ae64d37d3ea07004dde1483228d7927ca394730f
rabochiyclasss <20budkoivan02@gmail.com>
2026-04-28 17:44:15 +0200

    DI using @Qualifier

--- src/main/java/rabochiyclasss/dev/Main.java
+++ src/main/java/rabochiyclasss/dev/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(TaskConfiguration.class);
-//        Task task = context.getBean(Task.class);
-//        Task task2 = (Task) context.getBean("task");
+        Task mainTask = (Task) context.getBean("main-task");
+        Task notMainTask = (Task) context.getBean("not-main-task");
 //        System.out.println(task);
 //        System.out.println(task2);
 //        System.out.println(task == task2);
--- src/main/java/rabochiyclasss/dev/TaskConfiguration.java
+++ src/main/java/rabochiyclasss/dev/TaskConfiguration.java
@@ -1,20 +1,26 @@
 package rabochiyclasss.dev;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TaskConfiguration {
 
-//    @Bean
-//    public Task task() {
-//        return new Task();
-//    }
+    @Bean("main-task")
+    public Task task() {
+        return new Task();
+    }
+
+    @Bean("not-main-task")
+    public Task task2() {
+        return new Task();
+    }
 
     @Bean
     public TaskManager taskManager(
-            @Autowired(required = false) Task task
+           @Qualifier("main-task") Task task
     ){
         return new TaskManager(task);
     }
